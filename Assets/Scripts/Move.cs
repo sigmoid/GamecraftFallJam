0919189fcd4bb7cf6d768b7cf3ab07c746032be3
Adam Waggoner <adam.swaggoner@gmail.com>
2016-11-19 01:38:04 -0700

    Made nutz more random

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -7,12 +7,12 @@ public class Move : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+        transform.rotation = Random.rotation;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         transform.position += Speed * Time.deltaTime * Vector3.down;
-        transform.Rotate(Vector3.right * 50 * Time.deltaTime);
+        transform.Rotate(Vector3.right * 70 * Time.deltaTime);
 	}
 }
