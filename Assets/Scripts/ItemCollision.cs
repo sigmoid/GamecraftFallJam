b46575bbc0d573641911faa7f3a740d81582d566
Adam Waggoner <adam.swaggoner@gmail.com>
2016-11-19 01:16:06 -0700

    Added score pop

--- Assets/Scripts/ItemCollision.cs
+++ Assets/Scripts/ItemCollision.cs
@@ -6,6 +6,7 @@ public class ItemCollision : MonoBehaviour {
     public int points;
     public int damage;
 
+    public GameObject scorePop;
     public AudioClip CollectSound;
 
     void OnTriggerEnter(Collider other)
@@ -15,6 +16,8 @@ public class ItemCollision : MonoBehaviour {
             GameController.Game.AudioPlayer.PlayOneShot(CollectSound);
             GameController.Game.AddPoints(points);
             GameController.Game.SubtractLives(damage);
+            GameObject oth = Instantiate(scorePop, new Vector3(transform.position.x, transform.position.y, transform.position.z-3), new Quaternion()) as GameObject;
+            oth.GetComponent<ShowPoints>().ShowPointsPop(points);
             Destroy(gameObject);
         }        
     }
---
+++ Assets/Scripts/ShowPoints.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ShowPoints : MonoBehaviour {
+
+    public float time;
+    private Text text;
+    private SpriteRenderer sprite;
+
+    public Sprite plus5;
+    public Sprite plus10;
+
+	// Use this for initialization
+	void Start () {
+        sprite = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time -= Time.deltaTime;
+        if (sprite.color.a > 0)
+        {
+            sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, sprite.color.a - .05f);
+        }
+        if (time <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void ShowPointsPop(int points)
+    {
+        sprite = GetComponent<SpriteRenderer>();
+
+        if (points == 10)
+        {
+            sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, 1);
+            sprite.sprite = plus10;
+        }
+        else if (points == 5)
+        {
+            sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, 1);
+            sprite.sprite = plus5;            
+        }
+        else {
+            sprite.color = new Color(0, 0, 0, 0);
+        }
+    }
+
+
+}
