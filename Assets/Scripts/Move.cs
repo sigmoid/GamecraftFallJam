4d46ef259d589a8f3acf40973f4b8fadd1f8bbf2
Adam Waggoner <adam.swaggoner@gmail.com>
2016-11-18 19:57:02 -0700

    Made Object spawner

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+    public static float Speed = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += Speed * Time.deltaTime * Vector3.down;
+	
+	}
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+    public float Interval;
+    public Transform[] positions;
+    public GameObject Acorn;
+    public GameObject Bomb;
+
+    private float Timer;
+
+	// Use this for initialization
+	void Start () {
+        Timer = Interval;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Timer -= Time.deltaTime;
+        if (Timer <= 0)
+        {
+            Timer = Interval;
+            SpawnAcorns();
+        }
+	
+	}
+
+    void SpawnAcorns()
+    {
+        bool allBombs = true;
+        GameObject BombInstance;
+        for (int i = 0; i < positions.Length; i++) 
+        {
+            float randNum = Random.Range(0, 100);
+            if (randNum < 33)
+            {
+                Instantiate(Acorn, positions[i].position, Quaternion.identity);
+                allBombs = false;
+            }
+            else if (randNum < 50)
+            {
+                BombInstance = Instantiate(Bomb, positions[i].position, Quaternion.identity)as GameObject;
+                if (allBombs == true && i == 2)
+                {
+                    Destroy(BombInstance);
+                    Debug.Log("Destroyed Bombs");
+                }
+            }
+            else
+            {
+                allBombs = false;
+            }
+        }
+    }
+}
